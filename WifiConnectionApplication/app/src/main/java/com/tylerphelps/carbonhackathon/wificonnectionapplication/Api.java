67636b367270d1efb574fb3227feab6be60be91f
connorbkirk <Ansley524>
2017-06-07 15:37:57 -0400

    Update URLs

---
+++ WifiConnectionApplication/app/src/main/java/com/tylerphelps/carbonhackathon/wificonnectionapplication/Api.java
@@ -0,0 +1,71 @@
+package com.tylerphelps.carbonhackathon.wificonnectionapplication;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gtr126 on 6/7/17.
+ */
+
+public final class Api extends Activity {
+    private static String baseUrl;
+    private static RequestQueue queue;
+
+    public Api(){
+        baseUrl = "74.208.84.27:4000/";
+        queue = Volley.newRequestQueue(this);
+    }
+
+    /**
+     *
+     * @param lat latitude
+     * @param lon longitude
+     * @return List in form [ssid, password]
+     */
+    public static void getWifiInfo(double lat, double lon){
+        String url = baseUrl + "location?latitude="+lat+"&longitude="+lon;
+
+        // prepare the Request
+        final JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        try {
+                            String ssid = response.getString("ssid");
+                            String password = response.getString("password");
+                            //call chris/stephens procedure to log in
+                            //joinWifi(ssid, password);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.getLocalizedMessage());
+                    }
+                }
+        );
+
+        // add it to the RequestQueue
+        queue.add(getRequest);
+    }
+}
