121a44ded06803172de10697e6b3cd1e2b5efa16
Phelps17 <Phelps3@wisc.edu>
2017-06-07 15:33:41 -0500

    moved from activity into java controller

--- WifiConnectionApplication/app/src/main/java/com/tylerphelps/carbonhackathon/wificonnectionapplication/LocationServiceController.java
+++ WifiConnectionApplication/app/src/main/java/com/tylerphelps/carbonhackathon/wificonnectionapplication/LocationServiceController.java
@@ -3,36 +3,45 @@ package com.tylerphelps.carbonhackathon.wificonnectionapplication;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.model.LatLng;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.location.Location;
-import android.support.annotation.NonNull;
-import com.google.android.gms.location.LocationListener;
+import android.app.Activity;
 
 /**
  * Created by TylerPhelps on 6/7/17.
  */
 
-public class LocationServiceController  {
+public class LocationServiceController implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
     private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
-    private Context context;
-    private double latitude, longitude;
 
-    public static final String TAG = LocationServiceController.class.getSimpleName();
+    /**
+     * Represents a geographical location.
+     */
+    private Location mLastLocation;
+    private double latitude;
+    private double longitude;
+    private Activity parentActivity;
 
-    public LocationServiceController(Context context) {
+    private static final String TAG = "LOCATION CONTROLLER";
+
+    public LocationServiceController(Activity parentActivity) {
         this.latitude = 0;
         this.longitude = 0;
-        this.context = context;
+        this.parentActivity = parentActivity;
+
+        buildGoogleApiClient();
     }
+
     public double getLatitude() {
         return this.latitude;
     }
@@ -41,5 +50,72 @@ public class LocationServiceController  {
         return this.longitude;
     }
 
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    private synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this.parentActivity)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
 
+    public void connect() {
+        mGoogleApiClient.connect();
+    }
+
+    public void disconnect() {
+        mGoogleApiClient.disconnect();
+    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        if (ActivityCompat.checkSelfPermission(this.parentActivity,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this.parentActivity,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            this.latitude = mLastLocation.getLatitude();
+            this.longitude = mLastLocation.getLongitude();
+            Log.i(TAG, "Lat: " + latitude + ", Long: " + longitude);
+        } else {
+            Log.i(TAG, "No location detected");
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
 }
